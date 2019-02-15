91a124c033d698cee21b118d9d47582e42ff65d6
ElkhyatiBouchra <bouchra.elkh@gmail.com>
2019-02-15 11:45:22 +0800

    third commit

--- app.py
+++ app.py
@@ -16,23 +16,9 @@ app.config['SECRET_KEY'] = 'Thisissupposedtobesecrpet'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////Users/Elkhyati bouchra/Documents/appFlask/database.db'
 Bootstrap(app)  
 db = SQLAlchemy(app)
+from model import User , LoginForm ,RegisterForm
 
-### models
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique=True)
-    email = db.Column(db.String(50), unique=True)
-    password =db.Column(db.String(80))
 
-class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(),Length(min=4,max=15)])
-    password = PasswordField('password',validators=[InputRequired(),Length(min=8,max=80)] )
-    remember = BooleanField('remember me')
-
-class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(),Email(message='Invalid email'),Length(max=50)])
-    username = StringField('username',validators=[InputRequired(),Length(min=4,max=15)])
-    password = PasswordField('password',validators=[InputRequired(),Length(min=8,max=80)] )   
 
  #### mapping   
     
